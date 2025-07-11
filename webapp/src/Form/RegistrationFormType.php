86d796020df214b694d7151d2ed5a5269d13da4d
Alena Obraztsova <alena.obraztsova@brotherhood.software>
2025-07-11 13:39:26 +0700

    Поменяла deprecated формат валидации в форме (на named arguments, иначе в тестах deprecations)

--- webapp/src/Form/RegistrationFormType.php
+++ webapp/src/Form/RegistrationFormType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
 
@@ -15,22 +17,34 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'constraints' => [
+                    new Email(
+                        message: 'Email incorrect',
+                    ),
+                ]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    new NotBlank(
+                        message: 'Please enter a password',
+                    ),
+                    new Length(
+                        min:  6,
+                        minMessage: 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                        max: 4096,
+                    ),
                 ],
             ])
-            ->add('fullName');
+            ->add('fullName', TextType::class, [
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Please enter your full name',
+                    ),
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
